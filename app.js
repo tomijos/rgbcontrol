728168a85210b5cca862679f78c4d399bfac9c2c
tomijos <w_contreras@outlook.com>
2023-06-26 18:31:56 -0600

    update

--- app.js
+++ app.js
@@ -7,35 +7,39 @@
     const textAzul = document.querySelector('#texto-azul');
     const resultado = document.querySelector('#resultado');
 
-    let rojo = inputRojo.value;
-    let verde = inputVerde.value;
-    let azul = inputAzul.value;    
-    textRojo.textContent = `Código: ${rojo}`;
-    textVerde.textContent = `Código: ${verde}`;
-    textAzul.textContent = `Código: ${azul}`;
+    
+        let rojo = inputRojo.value;
+        let verde = inputVerde.value;
+        let azul = inputAzul.value;    
+        textRojo.textContent = `Código: ${rojo}`;
+        textVerde.textContent = `Código: ${verde}`;
+        textAzul.textContent = `Código: ${azul}`;
+        updateColor();
+    
+        inputRojo.addEventListener('input', (e) =>{        
+            rojo = e.target.value;
+            updateColor(rojo);               
+            
+        });
+        inputVerde.addEventListener('input', (e) =>{        
+            verde = e.target.value;
+            updateColor(verde);       
+            
+        });
+        inputAzul.addEventListener('input', (e) =>{        
+            azul = e.target.value;
+            updateColor(azul);      
+        });  
+    
+        function updateColor(){        
+             textRojo.textContent = `Código: ${rojo}`;
+             textVerde.textContent = `Código: ${verde}`;
+             textAzul.textContent = `Código: ${azul}`;
+             const colorRGB = `RGB(${rojo}, ${verde}, ${azul})`;
+             resultado.textContent =`RGB(${rojo}, ${verde}, ${azul})`;
+             document.body.style.backgroundColor = colorRGB;      
+    
+        }
+    
 
-
-    inputRojo.addEventListener('input', (e) =>{        
-        rojo = e.target.value;
-        updateColor(rojo);               
-        
-    });
-    inputVerde.addEventListener('input', (e) =>{        
-        verde = e.target.value;
-        updateColor(verde);       
-        
-    });
-    inputAzul.addEventListener('input', (e) =>{        
-        azul = e.target.value;
-        updateColor(azul);      
-    });  
-
-    function updateColor(){        
-         textRojo.textContent = `Código: ${rojo}`;
-         textVerde.textContent = `Código: ${verde}`;
-         textAzul.textContent = `Código: ${azul}`;
-         const colorRGB = `RGB(${rojo}, ${verde}, ${azul})`;
-         resultado.textContent =`RGB(${rojo}, ${verde}, ${azul})`;
-         document.body.style.backgroundColor = colorRGB;      
-
-    }
+    
